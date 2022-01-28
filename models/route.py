dd6377135a8fd3ccb7052c711f5110cc248fe3df
CodeClan Student <codeclanstudent@CodeClans-MacBook-Pro.local>
2022-01-28 12:21:57 +0000

    classes made

---
+++ models/route.py
@@ -0,0 +1,9 @@
+class Route:
+
+    def __init__(self, name, grade, crag, climbed = False, id = None):
+        self.name = name
+        self.grade = grade
+        self.crag = crag
+        self.climbed = climbed
+        self.id = id
+        
