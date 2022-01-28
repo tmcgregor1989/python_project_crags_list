5770d1f75c8ac713e8e3b36aed7b341d8809c985
CodeClan Student <codeclanstudent@CodeClans-MacBook-Pro.local>
2022-01-28 14:46:21 +0000

    crag repository written

---
+++ repositories/crag_repository.py
@@ -0,0 +1,58 @@
+from db.run_sql import run_sql
+from models.crag import Crag
+from models.location import Location
+from models.route import Route
+
+def save(crag):
+    sql = "INSERT INTO crags (name) VALUES (%s) RETURNING *"
+    values = [crag.name]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    crag.id = id
+    return crag
+
+def select_all():
+    crags = []
+    sql = "SELECT * FROM crags"
+    results = run_sql(sql)
+
+    for row in results:
+        crag = Crag(row['name'], row['id'])
+        crags.append(crag)
+    return crags
+
+def select(id):
+    crag = None
+    sql = "SELECT * FROM crags WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        crag = Crag(result['name'], result['id'])
+    return crag
+
+def delete_all():
+    sql = "DELETE FROM crags"
+    run_sql(sql)
+
+def delete(id):
+    sql = "DELETE FROM crags WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
+
+def update(crag):
+    sql = "UPDATE crags SET (name) = (%s) WHERE id = %s"
+    values = [crag.name, crag.id]
+    run_sql(sql, values)
+
+def routes(crag):
+    routes = []
+
+    sql = "SELECT * FROM routes WHERE crag_id = %s"
+    values = [crag.id]
+    results = run_sql(sql, values)
+
+    for row in results:
+        route = Route(row['name'], row['grade'], row['crag_id'], row['climbed'], row['id'])
+        routes.append(route)
+    return routes
---
+++ repositories/route_repository.py
@@ -0,0 +1,6 @@
+from db.run_sql import run_sql
+from models.crag import Crag
+from models.location import Location
+from models.route import Route
+import repositories.crag_repository as crag_repository
+import repositories.location_repository as location_repository
